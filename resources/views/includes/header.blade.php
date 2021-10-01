c18fd65231ef989dd6deb43c9a9708468e3b8167
mirkone1994 <mirkochiappori@gmail.com>
2021-10-01 16:22:22 +0200

    header done

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,11 +1,20 @@
 <div class="container">
     <div class="row">
         <div class="col">
-            <img src="../assets/img/dc-logo.png" alt="">
+            <img src={{ asset('images/dc-logo.png')}} alt="">
         </div>
         <div class="col">
             <ul class="navbar">
-                <li v-for="(link, index) in links" :key=index><a :href="link.url" :class="{active: link.current}">{{link.text}}</a></li>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="{{ route('home') }}">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
             </ul>
         </div>
     </div>
